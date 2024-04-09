4698af61650822a44e4a0a2c49fc6720f5f9917b
André Alcântara <106185003+a-alca@users.noreply.github.com>
2024-04-09 17:54:18 -0300

    Apliquei animacoes no elemento form, e para isso, criei os estados que chamei de shown e notShown, mas nao funcionou pois esse form eh um elemento  que nao esta anexado ao DOM, entao utilizei os estados void e conringa (*) e ainda refatorei esse codigo com os alias :enter e :leave

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -15,3 +15,31 @@ export const highlightedStateTrigger = trigger('highlightedState', [
     animate(350)
   ])
 ])
+
+export const shownStateTrigger = trigger('shownState', [
+// O METODO STATE QUE CHAMEI DE 'notshown' NAO FUNCIONOU PARA TRANSICAO ANIMADA DE ABERTURA DO FORM, POIS ELE NAO ESTA CARREGADO NO DOM, JA QUE ELE EH UM NGIF PARA RESONDER AO CLIQUE, POR ISSO COMENTEI ESSE CODIGO E SOLUCIONEI NO METODO TRANSITION COM O ESTADO VOID.
+    // state('notShown', style({ })),
+
+// STATE('SHOWN') RETIRADO PARA UTILIZACAO DO ESTADO CORINGA: * ONDE SUBSTITUI O SHOWN POR * NAS TRANSICOES COM VOID COMENTADOS NAS LINHAS 26 E 37
+    // state('shown', style({ })),
+
+//QUANDO ESTAMOS NA TRANSICAO DO ESTADO VOID PARA O ESTADO CORINGA, PODERMOS USAR O ACUCAR SINTATICO :enter - linha 29
+  // transition('void => *', [])
+
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate(300, style({
+      opacity: 1
+    }))
+  ]),
+
+//QUANDO ESTAMOS NA TRANSICAO DO ESTADO CORINGA PARA O ESTADO VOID, PODEMOS USAR O ACUCAR SINTATICO :leave - linha 40
+  // transition('* => void', [
+  transition(':leave', [
+    animate(300, style({
+      opacity: 0
+    }))
+  ])
+])
